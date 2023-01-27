fe9a03eced5d57279f3a0a484cab2dce41e7bbd0
Alex <sasha8562@gmail.com>
2023-01-27 18:57:38 +0300

    folders done

---
+++ Source/Platformer3D/Public/Platformer3DGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Platformer3DGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER3D_API UPlatformer3DGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
